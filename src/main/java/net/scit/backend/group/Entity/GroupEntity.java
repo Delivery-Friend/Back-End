d3658dd551eab12d0584c6d8711e4a344d2cce7b
PSJ <itpark1018@gmail.com>
2024-12-20 18:55:23 +0900

    (feat:init-structure)
    저희 프로젝트의 구조 입니다.
    모든 서비스는 이런한 구조로 개발하시면 될 것 같습니다.

---
+++ src/main/java/net/scit/backend/group/Entity/GroupEntity.java
@@ -0,0 +1,27 @@
+package net.scit.backend.group.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "group")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GroupEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "group_id")
+    private Long id;
+
+    @Column(name = "member_id")
+    private MemberEntity member;
+
+    private String groupName;
+    private LocalDate createDate;
+}
---
+++ src/main/java/net/scit/backend/group/service/GroupService.java
@@ -0,0 +1,7 @@
+package net.scit.backend.group.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface GroupService {
+}
