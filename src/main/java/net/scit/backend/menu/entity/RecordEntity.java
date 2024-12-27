6f84cc98ec070fb622ba29239b2e880bcd075a8a
Your Name <bokyung99817@gmail.com>
2024-12-27 17:48:23 +0900

    createmenu

---
+++ src/main/java/net/scit/backend/menu/entity/RecordEntity.java
@@ -0,0 +1,18 @@
+package net.scit.backend.menu.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+
+@Entity
+//@Table(name="")
+public class RecordEntity {
+}
---
+++ src/main/java/net/scit/backend/menu/service/RecommendService.java
@@ -0,0 +1,9 @@
+package net.scit.backend.menu.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+// @RequiredArgsConstructor
+public interface RecommendService {
+}
