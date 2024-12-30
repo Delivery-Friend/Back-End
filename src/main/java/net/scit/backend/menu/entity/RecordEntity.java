4e1a92d9b4661e999f29ae1f77117be477603d66
Your Name <bokyung99817@gmail.com>
2024-12-30 17:22:24 +0900

    Nmenu

--- src/main/java/net/scit/backend/menu/entity/RecordEntity.java
+++ src/main/java/net/scit/backend/menu/entity/RecordEntity.java
@@ -1,11 +1,14 @@
 package net.scit.backend.menu.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,4 +18,28 @@ import lombok.Setter;
 @Entity
 //@Table(name="")
 public class RecordEntity {
+
+    private Long storeId;
+
+    @Column(name="store_name")
+    private String storeName;
+
+    @Column(name="store_address")
+    private  String storeAddress;
+
+    @Column(name="menu_name")
+    private String menuName;
+
+    @Column(name="price")
+    private int price;
+
+    @Column(name="cnt")
+    private int cnt;
+
+    @Column(name="order_date")
+    private LocalDate orderDate;
+
+    @Column(name="is_group")
+    private boolean isGroup;
+
 }
