cf52bde6d486d68d2efbdb0c3dd0e683e7c880bf
Your Name <bokyung99817@gmail.com>
2025-01-03 17:23:24 +0900

    feat/menu

---
+++ src/main/java/net/scit/backend/menu/dto/StoreDTO.java
@@ -0,0 +1,24 @@
+package net.scit.backend.menu.dto;
+
+import lombok.*;
+import net.scit.backend.menu.entity.StoreEntity;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class StoreDTO {
+    private Long storeId;
+    private String storeName;
+    private String storeAddress;
+
+    public static StoreDTO toDTO(StoreEntity storeEntity){
+        return StoreDTO.builder()
+                .storeId(storeEntity.getStoreId())
+                .storeName(storeEntity.getStoreName())
+                .storeAddress(storeEntity.getStoreAddress())
+                .build();
+    }
+}
