04b5d04f222aeffddee400de8a8c4ac6ea226043
yewon <ipunwon_@naver.com>
2025-01-07 17:20:23 +0900

    fixed untracked files

--- src/main/java/net/scit/backend/member/dto/TokenDTO.java
+++ src/main/java/net/scit/backend/member/dto/TokenDTO.java
@@ -1,4 +1,14 @@
 package net.scit.backend.member.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class TokenDTO {
+
+    String accessToken;
+    String refreshToken;
 }
