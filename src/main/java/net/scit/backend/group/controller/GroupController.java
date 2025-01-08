116f01d30e13ca92aa50db6bc0464ba1d958bcd1
eunjini <ckatkfkd725@naver.com>
2025-01-08 18:26:54 +0900

    (feat:create-group)
    그룹 생성 기능을 만들었습니다.
    토큰과 그룹 이름을 받아서 그룹을 생성합니다.

--- src/main/java/net/scit/backend/group/controller/GroupController.java
+++ src/main/java/net/scit/backend/group/controller/GroupController.java
@@ -6,19 +6,24 @@ import net.scit.backend.group.dto.MyGroupDTO;
 import net.scit.backend.group.service.GroupService;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/group")
 @RequiredArgsConstructor
 public class GroupController {
     private final GroupService groupService;
+
+    @PostMapping("/create")
+    public ResponseEntity<Map<String, Object>> createGroup(HttpServletRequest request,
+                                                           @RequestParam String groupName) {
+        // 그룹 생성
+        Map<String, Object> result = groupService.createGroup(request, groupName);
+        return ResponseEntity.ok(result);//생성된 결과를 클라이언트에게 반환
+    }
 
     @GetMapping("/list")
     public ResponseEntity<Page<MyGroupDTO>> getGroupList(HttpServletRequest request, @RequestParam(name = "page", defaultValue = "0") Integer page) {
--- src/main/java/net/scit/backend/group/service/impl/GroupServiceImpl.java
+++ src/main/java/net/scit/backend/group/service/impl/GroupServiceImpl.java
@@ -2,9 +2,12 @@ package net.scit.backend.group.service.impl;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import net.scit.backend.group.dto.GroupDTO;
 import net.scit.backend.group.dto.MyGroupDTO;
+import net.scit.backend.group.entity.GroupEntity;
 import net.scit.backend.group.entity.GroupMemberEntity;
 import net.scit.backend.group.repository.GroupMemberRepository;
+import net.scit.backend.group.repository.GroupRepository;
 import net.scit.backend.group.service.GroupService;
 import net.scit.backend.member.entity.MemberEntity;
 import net.scit.backend.member.jwt.JWTUtil;
@@ -15,15 +18,50 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class GroupServiceImpl implements GroupService {
     private final MemberRepository memberRepository;
+    private final GroupRepository groupRepository;
     private final GroupMemberRepository groupMemberRepository;
     private final JWTUtil jwtUtil;
+
+    @Override
+    public Map<String, Object> createGroup(HttpServletRequest request, String groupName) {
+        // 1. 토큰 처리
+        String token = jwtUtil.resolveToken(request);
+        String email = jwtUtil.getEmail(token);
+
+        Optional<MemberEntity> byEmail = memberRepository.findByEmail(email);
+        if (byEmail.isEmpty()) {
+            // CustomException 으로 교체 예정
+            throw new RuntimeException("not found member");
+        }
+
+        MemberEntity member = byEmail.get();
+
+        // 2. GroupDTO -> GroupEntity로 변환
+        GroupDTO groupDTO = GroupDTO.builder()
+                .member(member)
+                .groupName(groupName)
+                .createDate(LocalDate.now())
+                .build();
+        GroupEntity groupEntity = GroupEntity.toEntity(groupDTO);
+
+        // 3. 데이터베이스에 저장
+        groupRepository.save(groupEntity);
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("success", true);
+
+        return result;
+    }
 
     @Override
     public Page<MyGroupDTO> getGroupList(HttpServletRequest request, Integer page) {
